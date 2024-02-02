4d27274ef4641ff2fc20e6843b51baff911f0dc7
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-02 15:55:12 +0100

    ajout méthode listeActeurs

--- Role.php
+++ Role.php
@@ -2,6 +2,7 @@
 
 class Role{
     private string $_nomRole;
+    private array $_listeActeurs;
 
     public function __construct(string $nomRole){
         $this->_nomRole = $nomRole;
@@ -21,11 +22,24 @@ class Role{
     }
 
     // --------------------------------------- AFFICHAGE -------------------------------------------------
+
     public function __toString(){
         return "$this->_nomRole";
     }
 
+    public function afficherListeActeurs(){
+        $result= "Acteurs ayant incarné $this : <br>-------------------------------------<br>";
+        foreach ($this->_listeActeurs as $acteur){
+            $result.=$acteur."<br>";
+        }
+        $result.= "<br><br>";
+        return $result;
+    }
+
     // --------------------------------------- METHODES CUSTOM -------------------------------------------------
 
+    public function fillListeActeurs(Casting $acteur){
+        $this->_listeActeurs[]= $acteur->afficherActeurFilm();
+    }
 
 }
